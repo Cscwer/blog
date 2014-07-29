4f5c919d8ac459c1a97579ed692363dcac7c6740
Jeon <asdf3601812@163.com>
2014-07-30 00:31:04 +0800

    Renew
    Renew Push

---
+++ js/style.js
@@ -0,0 +1,52 @@
+var key = 1;
+var lock = 0;
+$('#leader').click(function() {
+  if(key == 0) {
+    $('ul.lead').css('right', '80px');
+    $('ul.lead').css('color', 'rgba(250, 120, 20, 1)');
+    //$('div.container').css('background-color', 'rgba(0, 0, 0, 0.2)');
+    key = 1;
+  }
+  else
+  if(key == 1) {
+    $('ul.lead').css('right', '-150px');
+    $('ul.lead').css('color', 'rgba(250, 120, 20, 0)');
+   // $('div.container').css('background-color', 'rgba(0, 0, 0, 0)');
+    key = 0;
+  }
+}
+);
+$('#leader').click(function() {
+  if(lock == 0) {
+    $('img.menu').addClass("transform ");
+    lock = 1;
+  }
+  else
+  if(lock == 1) {
+    $('img.menu').removeClass("transform ");
+    lock = 0;
+  }
+}
+);
+$('#leader').mouseover(function() {
+  $('div#leader').css('background-color', 'rgba(64, 64, 64, 1)');
+}
+);
+$('#leader').mouseout(function() {
+  $('div#leader').css('background-color', 'rgba(64, 64, 64, 1)');
+}
+);
+$("header").data("headroom").destroy();
+// 按钮操作
+$('button#selector').mouseover(function(){
+    $('ul#inner').css('display','inline');
+  });
+  $('ul#inner').mouseover(function(){
+    $('ul#inner').css('display','inline');
+  });
+  $('button#selector').mouseout(function(){
+    $('ul#inner').css('display','none');
+  });
+  $('ul#inner').mouseout(function(){
+    $('ul#inner').css('display','none');
+  });
